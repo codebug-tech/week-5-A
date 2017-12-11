79f9d1c0b0f571052f10f720fafb311652516ae3
mcarthurgill <mcarthur.gill@gmail.com>
2017-12-10 20:01:47 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+/////////////////////WRITE YOUR VARIABLES BELOW//////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+///////////////YOUR VARIABLES SHOULD BE ABOVE THIS LINE////////////////////
+
+document.addEventListener('DOMContentLoaded', function() {
+
+  alert("Hi my name is " + myName + ". I am " + myAge + " years old, and my favorite color is " + myFavoriteColor + ". My favorite hobby is " + myFavoriteHobby + "!")
+
+})
