dd77ce376a918434549b627a76d23f9e840b8d49
Brandon Sorbara <sorbarbj@Brandons-MBP-2.home>
2017-12-12 20:27:55 -0500

    added variables for alert

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 /////////////////////WRITE YOUR VARIABLES BELOW//////////////////////////
-
-
-
+var myName = "Brandon";
+var myAge = 26;
+var myFavoriteColor = "purple";
+var myFavoriteHobby = "playing sports";
+var myEmployer = "American Express"
+var myPosition = "Analyst"
 
 
 
@@ -14,6 +17,6 @@
 
 document.addEventListener('DOMContentLoaded', function() {
 
-  alert("Hi my name is " + myName + ". I am " + myAge + " years old, and my favorite color is " + myFavoriteColor + ". My favorite hobby is " + myFavoriteHobby + "!")
+  alert("Hi my name is " + myName + ". I am " + myAge + " years old, and my favorite color is " + myFavoriteColor + ". My favorite hobby is " + myFavoriteHobby + "!" + " I currently work at " + myEmployer + " as an " + myPosition + ".")
 
 })
